52afc394e18ae68e32d15b2d367a8a21afcd2526
pauwma <pauwmaa@gmail.com>
2022-11-01 14:10:55 +0100

    1 (Errores al entrar a los posts)

--- src/main/java/Scrapper.java
+++ src/main/java/Scrapper.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
+import javax.swing.text.Element;
 import java.util.List;
 
 public class Scrapper {
@@ -18,7 +19,28 @@ public class Scrapper {
     public void getPosts(){
         driver.get("https://www.spacelaunchschedule.com");
         List<WebElement> posts = driver.findElements(By.className("my-2"));
+        getInfoPostInside(posts);
+    }
 
+
+    public void getInfoPostInside(List<WebElement> posts){
+        for (WebElement post: posts){
+            String urlPost = post.findElement(By.className("stretched-link")).getAttribute("href");
+            driver.navigate().to(urlPost);
+
+            String nombreMision = driver.findElement(By.className("entry-title")).getText();
+            String coheteMision = driver.findElement(By.className("mt-2")).getText();
+            String compañiaMision = driver.findElement(By.className("mb-3")).getText();
+            String rocketInfo = driver.findElement(By.id("rocket")).getText();
+
+            System.out.println("--------------------------\n"
+                    + nombreMision + " - " + coheteMision + " - " + compañiaMision + "\n" +
+                    rocketInfo);
+            driver.navigate().to("https://www.spacelaunchschedule.com");
+        }
+    }
+
+    public void getInfoPost(List<WebElement> posts){
         for (WebElement post: posts){
             String nombreMision = post.findElement(By.className("entry-title")).getText();
             nombreMision = nombreMision.split("\n")[0];
