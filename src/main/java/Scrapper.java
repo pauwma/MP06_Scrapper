9433876033ac5a7540b6dcee53c180ce1fb549a1
pauwma <pauwmaa@gmail.com>
2022-11-14 18:19:51 +0100

    Push 2

--- src/main/java/Scrapper.java
+++ src/main/java/Scrapper.java
@@ -35,27 +35,28 @@ public class Scrapper {
     }
 
     public void getInfoPost(List<String> allURLs) throws IOException {
-        for (String url: allURLs){
-            driver.get(url);
-        }
+        writerCSV(allURLs);
     }
 
     public void launchToCSV(BufferedWriter bf) throws IOException {
         WebElement mission = driver.findElement(By.id("mission"));
-        String mission_title = mission.findElement(By.className("h5")).getText();
-        String mission_desc = mission.findElement(By.xpath("//*[@id=\"mission\"]/div/div[1]/p[1]")).getText();
+        String launch_title = "", launch_status = "", launch_date = "", rocket_name = "", agency_date = "", location_name = "";
+        try {launch_title = mission.findElement(By.className("h5")).getText();} catch (Exception e){}
+        try {launch_status = mission.findElement(By.xpath("//*[@id=\"launch-details\"]/div[2]/div/div[2]")).getText();} catch (Exception e){}
+        try {launch_status = mission.findElement(By.xpath("//*[@id=\"launch-details\"]/div[3]/div/div[2]/time/text()[1]")).getText();} catch (Exception e){}
 
-        bf.write("\"launch_name\",\"launch_status\",\"launch_date\",\"mision_name\",\"rocket_name\",\"agency_name\",\"location_name\"");
-        bf.write("\n\"" + mission_title + "\",\"" + mission_desc + "\"");
+        bf.write("\n\"" + launch_title + "\",\"" + launch_status + "\",\"" + launch_date);
     }
 
     public void writerCSV(List<String> allURLs) throws IOException {
         FileWriter fw = new FileWriter("out/launch.csv");
         BufferedWriter bf = new BufferedWriter(fw);
-
         for (String url : allURLs){
             driver.get(url);
+            launchToCSV(bf);
         }
+        bf.close();
+
     }
 
 }
