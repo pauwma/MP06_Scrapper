ab9dc41c86580d1b468f8f9aa42f4c450ede96b2
pauwma <pauwmaa@gmail.com>
2022-11-15 11:27:17 +0100

    Launch CSV - Completado

--- src/main/java/Scrapper.java
+++ src/main/java/Scrapper.java
@@ -18,6 +18,7 @@ public class Scrapper {
         driver = new FirefoxDriver(options);
     }
 
+    // ? Obtiene todos los posts de la primera página.
     public void getPosts() throws IOException {
         driver.get("https://www.spacelaunchschedule.com");
         List<WebElement> posts = driver.findElements(By.className("my-2"));
@@ -26,6 +27,7 @@ public class Scrapper {
         getInfoPost(allURLs);
     }
 
+    // ? Obtiene todos los CSV de todos los post.
     public List<String> getAllURL(List <WebElement> posts){
         List<String> URLs = new ArrayList<>();
         for(WebElement post : posts){
@@ -34,29 +36,57 @@ public class Scrapper {
         return URLs;
     }
 
+    // ? Obtiene todos los CSV de 1 post.
     public void getInfoPost(List<String> allURLs) throws IOException {
-        writerCSV(allURLs);
+        getLaunchesCSV(allURLs);
     }
 
-    public void launchToCSV(BufferedWriter bf) throws IOException {
-        WebElement mission = driver.findElement(By.id("mission"));
-        String launch_title = "", launch_status = "", launch_date = "", rocket_name = "", agency_date = "", location_name = "";
-        try {launch_title = mission.findElement(By.className("h5")).getText();} catch (Exception e){}
-        try {launch_status = mission.findElement(By.xpath("//*[@id=\"launch-details\"]/div[2]/div/div[2]")).getText();} catch (Exception e){}
-        try {launch_status = mission.findElement(By.xpath("//*[@id=\"launch-details\"]/div[3]/div/div[2]/time/text()[1]")).getText();} catch (Exception e){}
-
-        bf.write("\n\"" + launch_title + "\",\"" + launch_status + "\",\"" + launch_date);
-    }
-
-    public void writerCSV(List<String> allURLs) throws IOException {
+    // ? Escribe toda la información sobre Launch de todos los URLS.
+    public void getLaunchesCSV(List<String> URLS) throws IOException {
         FileWriter fw = new FileWriter("out/launch.csv");
         BufferedWriter bf = new BufferedWriter(fw);
-        for (String url : allURLs){
+        bf.write("\"launch_name\",\"launch_status\",\"launch_date\",\"rocket_name\",\"agency_name\",\"location_name\"\n");
+        for (String url : URLS){
             driver.get(url);
-            launchToCSV(bf);
+            getLaunchCSV(bf);
         }
         bf.close();
-
     }
+
+    // ? Escribe toda la información sobre Launch de 1 URLS.
+    public void getLaunchCSV(BufferedWriter bf) throws IOException {
+        String launch_title = "", launch_status = "", launch_date = "", rocket_name = "", agency_name = "", location_name = "";
+        WebElement launch_details = driver.findElement(By.id("launch-details"));
+        try {
+            launch_title = launch_details.findElement(By.className("entry-title")).getText();
+            launch_title.replace(",","");
+            System.out.println("INFO - launch_title");
+        } catch (Exception e){}
+        try {
+            launch_status = launch_details.findElement(new By.ByXPath("//*[@id=\"launch-details\"]/div[2]/div/div[2]")).getText();
+            launch_status = launch_status.replace("STATUS","").replace("LAUNCH","").replace("\n","").trim();
+            System.out.println("INFO - launch_status");
+        } catch (Exception e){}
+        try {
+            launch_date = launch_details.findElement(By.className("launchDateTime")).getText();
+            launch_date = launch_date.replace("\n"," - ").replace("•","").trim();
+            System.out.println("INFO - launch_date");
+        } catch (Exception e){}
+        try {
+            rocket_name = launch_details.findElement(By.className("mt-2")).getText();
+            System.out.println("INFO - rocket_name");
+        } catch (Exception e){}
+        try {
+            agency_name = launch_details.findElement(By.className("pb-3")).getText();
+            System.out.println("INFO - agency_name");
+        } catch (Exception e){}
+        try {
+            WebElement location = driver.findElement(By.id("location"));
+            location_name = location.findElement(By.className("stretched-link")).getText();
+            System.out.println("INFO - location_name");
+        } catch (Exception e){}
+        bf.write("\"" + launch_title + "\",\"" + launch_status + "\",\"" + launch_date + "\",\"" + rocket_name + "\",\"" + agency_name + "\",\"" + location_name + "\"\n");
+    }
+
 
 }
