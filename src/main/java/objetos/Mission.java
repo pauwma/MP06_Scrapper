08584d05ee9a3d302140e25c828e7eaf590c0c88
pauwma <114858315+pauwma@users.noreply.github.com>
2022-11-24 10:28:19 +0100

    Add files via upload

--- src/main/java/objetos/Mission.java
+++ src/main/java/objetos/Mission.java
@@ -1,14 +1,14 @@
 package objetos;
 
 public class Mission {
-    private String launch_name = "unknown";
+    private String rocket_name = "unknown";
     private String mission_name = "unknown";
     private String mission_type = "unknown";
     private String mission_launch_cost = "unknown";
     private String mission_description = "unknown";
 
-    public Mission(String launch_name, String mission_name, String mission_type, String mission_launch_cost, String mission_description) {
-        this.launch_name = launch_name;
+    public Mission(String rocket_name, String mission_name, String mission_type, String mission_launch_cost, String mission_description) {
+        this.rocket_name = rocket_name;
         this.mission_name = mission_name;
         this.mission_type = mission_type;
         this.mission_launch_cost = mission_launch_cost;
