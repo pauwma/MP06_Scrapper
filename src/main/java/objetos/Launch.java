8383a0d006e5c3eb2830a016bd31793355f014e0
pauwma <114858315+pauwma@users.noreply.github.com>
2022-12-01 19:35:22 +0100

    Add files via upload

--- src/main/java/objetos/Launch.java
+++ src/main/java/objetos/Launch.java
@@ -1,11 +1,20 @@
 package objetos;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Launch {
     private String launch_title = "unknown";
+
     private String launch_status = "unknown";
+
     private String launch_date = "unknown";
+
     private String rocket_name = "unknown";
+
     private String agency_name = "unknown";
+
     private String location_name = "unknown";
 
     public Launch(String launch_title, String launch_status, String launch_date, String rocket_name, String agency_name, String location_name){
@@ -15,5 +24,35 @@ public class Launch {
         this.rocket_name = rocket_name;
         this.agency_name = agency_name;
         this.location_name = location_name;
+    }
+
+    @XmlElement
+    public String getLaunch_title() {
+        return launch_title;
+    }
+
+    @XmlElement
+    public String getLaunch_status() {
+        return launch_status;
+    }
+
+    @XmlElement
+    public String getLaunch_date() {
+        return launch_date;
+    }
+
+    @XmlElement
+    public String getRocket_name() {
+        return rocket_name;
+    }
+
+    @XmlElement
+    public String getAgency_name() {
+        return agency_name;
+    }
+
+    @XmlElement
+    public String getLocation_name() {
+        return location_name;
     }
 }
